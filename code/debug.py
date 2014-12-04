c2e30000afb973772aed95d257c333da94f5d93e
fenglifeng <fenglifeng2010@163.com>
2014-12-04 10:57:40 +0800

    add readme AND delete debug info

--- code/debug.py
+++
@@ -1,4 +0,0 @@
-x = '123'
-a = "hello {f}".format()
-cmd  ="select SINum= {num}".format{num=x}
-print cmd
--- inverter/debug.py
+++
@@ -1,9 +0,0 @@
-import SocketServer 
-from YsLow10 import *
-from sqldb import *
-from Pysettimer import *
-from sip import *
-
-sql = sqldb()
-rows = sql.GetLowInverterList()
-print type(rows)
